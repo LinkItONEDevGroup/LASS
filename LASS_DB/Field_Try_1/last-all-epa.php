8c9c75406efcecd65c6d0587d937dbbee973229a
LASS <lass@lass.iis.sinica.edu.tw>
2016-01-15 00:14:47 +0800

    - show the latest data entry of each node/site

--- LASS_DB/Field_Try_1/last-all-epa.php
+++ LASS_DB/Field_Try_1/last-all-epa.php
@@ -2,30 +2,32 @@
 // Configuration
 $dbhost = 'lass.iis.sinica.edu.tw';
 $dbname = 'LASS';
-$device_id = $_GET['device_id'];
  
 // Connect to mongo database
 $mongoClient = new MongoClient('mongodb://' . $dbhost);
 $db = $mongoClient->$dbname;
 $collection = $db->posts;
 
+$date_today = date("Y-m-d");
+$date_yesterday = date("Y-m-d",time() - 60 * 60 * 24);
 
 
-$sites = $collection->distinct('SiteName', array('date'=> date("Y-m-d")));
+$sites = $collection->distinct('SiteName');
 
 $msgs = array();
 foreach ($sites as $site){
 
 	$items = $collection->find(array('SiteName' => $site, 
-				         'date'=>array('$lte'=> date("Y-m-d")))
-				  )->sort(array('date'=> -1, 'time'=>-1))->limit(1);
+                                'date' => array( '$in' => array($date_today, $date_yesterday))
+                                        )
+                                  )->sort(array('date'=> -1, 'time'=>-1))->limit(1);
 
 	$msg = array('SiteName' => $site);
 
 	foreach ($items as $item){
 		$timestamp = $item['date']."T".$item['time']."Z";
 		$msg['timestamp'] = $timestamp;
-		if (isset($item['PM2.5'])) $msg['PM2.5'] = $item['PM2.5'];
+		if (isset($item['PM2_5'])) $msg['PM2_5'] = $item['PM2_5'];
 		if (isset($item['PM10'])) $msg['PM10'] = $item['PM10'];
 		if (isset($item['SO2'])) $msg['SO2'] = $item['SO2'];
 		if (isset($item['NOx'])) $msg['NOx'] = $item['NOx'];
--- LASS_DB/Field_Try_1/last-all.php
+++ LASS_DB/Field_Try_1/last-all.php
@@ -1,48 +1,59 @@
 <?php
 // Configuration
-$dbhost = 'lass.iis.sinica.edu.tw';
+$dbhost = '140.109.21.181';
 $dbname = 'LASS';
-$device_id = $_GET['device_id'];
  
 // Connect to mongo database
 $mongoClient = new MongoClient('mongodb://' . $dbhost);
 $db = $mongoClient->$dbname;
 $collection = $db->posts;
 
+$date_today = date("Y-m-d");
+$date_yesterday = date("Y-m-d",time() - 60 * 60 * 24);
 
-
-$devices = $collection->distinct('device_id', array('date'=> date("Y-m-d")));
-
+$devices = $collection->distinct('device_id');
 $msgs = array();
+
 foreach ($devices as $device_id){
 
 	$items = $collection->find(array('device_id' => $device_id, 
-				         'date'=>array('$lte'=> date("Y-m-d")))
+				'date' => array( '$in' => array($date_today, $date_yesterday))
+//				'date' => $date_today
+					)
 				  )->sort(array('date'=> -1, 'time'=>-1))->limit(1);
-
+/*
+	$array_items = iterator_to_array($items);
+	if (!isset($array_items[0]['date'])){
+		$items = $collection->find(array('device_id' => $device_id, 
+						'date' => $date_yesterday
+						)
+					  )->sort(array('date'=> -1, 'time'=>-1))->limit(1);
+	$array_items = iterator_to_array($items);
+	}
+	//if (!isset($array_items[0]['date'])) continue;
+*/
+	if (count($items)==0) continue;
 	$msg = array('device_id' => $device_id);
-
 	foreach ($items as $item){
+		$s_date = $item['date'];
+		$s_time = $item['time'];
 		$timestamp = $item['date']."T".$item['time']."Z";
 		$msg['timestamp'] = $timestamp;
 		if (isset($item['s_d0'])) $msg['s_d0'] = $item['s_d0'];
 		if (isset($item['s_d1'])) $msg['s_d1'] = $item['s_d1'];
 		if (isset($item['s_t0'])) $msg['s_t0'] = $item['s_t0'];
 		if (isset($item['s_h0'])) $msg['s_h0'] = $item['s_h0'];
+		if (isset($item['gps_fix'])){
+			if ($item['gps_fix']==1 && isset($item['loc'])){
+				$location = $item['loc']['coordinates'];
+				$msg['gps_lat'] = $location[0];
+				$msg['gps_lon'] = $location[1];
+				$msg['gps_num'] = $item['gps_num'];
+			}
+		}
 	}
 
-	$items = $collection->find(array('device_id' => $device_id, 
-					 'gps_fix' => 1,
-				         'date'=>array('$lte'=> date("Y-m-d")))
-				  )->sort(array('gps_num' => -1, 'date'=> -1, 'time'=>-1))->limit(1);
-	foreach ($items as $item){
-		$location = $item['loc']['coordinates'];
-		$msg['gps_lat'] = $location[0];
-		$msg['gps_lon'] = $location[1];
-		$msg['gps_num'] = $item['gps_num'];
-	}
-
-	array_push($msgs, $msg);
+	if (isset($msg['gps_num'])) array_push($msgs, $msg);
 
 }
 
