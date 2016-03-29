21f2a63576b3417e982ea194d0b3e4dee3962c5d
wuulong <wuulong@gmail.com>
2016-03-30 07:13:33 +0800

    The IASS initial full contributions from Cheng Hsun Tseng@SunplusIT

---
+++ IASS/IAAS_CODES/sensors/FC-37/FC-37.ino
@@ -0,0 +1,37 @@
+
+#define waterPin A3
+
+
+class deviceFC37 {    
+  public:
+    deviceFC37() {
+      
+    }
+
+    float readValue() {
+       int sensorRead = analogRead(waterPin);
+       //return (sensorRead*100)/1023;
+       int ratio = 100-(sensorRead/10);
+       if(ratio<0) {
+          return 0;
+       }else if(ratio>100) {
+          return 100;
+       }else{
+          return ratio;
+       }
+    }
+};
+
+deviceFC37 sensorFC37;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("FC-37 test!");
+
+}
+
+void loop() {
+  Serial.println(sensorFC37.readValue());
+  delay(2000);
+    
+}  
---
+++ IASS/IAAS_CODES/sensors/MQ7/MQ7.ino
@@ -0,0 +1,28 @@
+
+#define gasMQ7Pin A5
+
+
+class deviceMQ7 {    
+  public:
+    deviceMQ7() {
+      
+    }
+
+    float readValue() {
+      return analogRead(gasMQ7Pin);
+    }
+};
+
+deviceMQ7 sensorMQ7;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("MQ7 test!");
+
+}
+
+void loop() {
+  Serial.println(sensorMQ7.readValue());
+  delay(2000);
+    
+}  
