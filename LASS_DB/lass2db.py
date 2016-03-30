36e4709d742271d47c1d8993ce2c11480fbf5a7d
robert501128 <robert501128@gmail.com>
2016-03-30 15:10:52 +0800

    change lass2db.py

--- LASS_DB/lass2db.py
+++ LASS_DB/lass2db.py
@@ -90,6 +90,7 @@ def on_message(client, userdata, msg):
     db_msg = "{"
     flag = 0
     app = 0
+    LASS_DEVICE_ID="non_device"
     for item in items:
         if item == '':
             continue 
